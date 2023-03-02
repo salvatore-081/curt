27e9b81a830134a2a3d14076d7593cf54c993ed2
Salvatore <s.emilio@reply.it>
2023-03-02 12:02:19 +0100

    fixed data path

--- internal/resolver.go
+++ internal/resolver.go
@@ -18,7 +18,7 @@ func (r *Resolver) Create(host string, apiKey string) (e error) {
 	r.Host = host
 	r.ApiKey = apiKey
 
-	r.BadgerDB, e = badger.Open(badger.DefaultOptions("../data").WithLogger(middlewares.BadgerLogger{}))
+	r.BadgerDB, e = badger.Open(badger.DefaultOptions("./data").WithLogger(middlewares.BadgerLogger{}))
 	if e != nil {
 		return e
 	}
