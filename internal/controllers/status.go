1601645e4c602e9ae0750d46e72f561d43f84358
Salvatore <s.emilio@reply.it>
2023-03-02 18:51:49 +0100

    promoted to final release

--- internal/controllers/status.go
+++ internal/controllers/status.go
@@ -49,7 +49,7 @@ func About(g *gin.RouterGroup, r *internal.Resolver) {
 
 		modules := []models.Module{{
 			Path:    "Curt",
-			Version: "1.1.0-rc.4",
+			Version: "1.1.0",
 		}}
 
 		for _, module := range info.Deps {
